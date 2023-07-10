1ec0670591f5eb8646187d367da1269787b94748
Claudio-Olivera <claudioolivera753@gmail.com>
2023-07-10 02:31:35 -0300

    end section 12

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 //import { MenuModule } from 'primeng/menu';
+
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 @NgModule({
@@ -17,6 +19,8 @@ import { PanelModule } from 'primeng/panel';
     FieldsetModule,
     MenubarModule,
     PanelModule,
+    TableModule,
+    ToolbarModule,
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ProductsRoutingModule } from './products-routing.module';
+import { SortByPipe } from './pipes/sort-by.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
@@ -14,12 +18,19 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+
+    //Pipes
+    CanFlyPipe,
+    SortByPipe,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    PrimeNgModule
+    PrimeNgModule,
+
   ]
 })
 export class ProductsModule { }
